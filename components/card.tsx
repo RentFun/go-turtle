c5da360dd5c14974a6dfc60c08096a57b7f2744f
kaelnew <kevin@rentfun.io>
2023-03-10 23:27:51 +0800

    game link updated

--- components/card.tsx
+++ components/card.tsx
@@ -8,7 +8,7 @@ function UserNftCard({ turtle, listType, cb }: { turtle: IUserNftWithMetadata, l
             case ListType.Mine:
                 return <Button variant="primary" onClick={async () => {lend(turtle.contract_, turtle.tokenId, zeroAddress, 1e15).then(() => {cb()})}}>Lend Out</Button>;
             case ListType.MyRental:
-                return <><Card.Text>LeftSeconds: {turtle.endTime}</Card.Text><Button href='http://game.rentfun.io' target='_blank'>Play Game</Button></>
+                return <><Card.Text>LeftSeconds: {turtle.endTime}</Card.Text><Button href='http://turtis.rentfun.io' target='_blank'>Play Game</Button></>
             case ListType.MyListed:
                 if (turtle.endTime > 0) {
                     return <><Card.Text>LeftSeconds: {turtle.endTime}</Card.Text>
