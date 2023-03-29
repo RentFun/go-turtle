0f1d3a7de5b8dedd763c385d2cc3de1c60d1a617
kaelnew <kevin@rentfun.io>
2023-03-29 11:44:27 +0800

    add Testing Guide

--- components/body.tsx
+++ components/body.tsx
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Link from 'next/link';
 import { init, isAuth } from "@/lib/Web3Client";
 import { useState, useEffect } from 'react';
 
@@ -28,6 +29,11 @@ const Body = () => {
             <Container className="p-4">
             <Row>
                 <Col>RentFun MarketPlace</Col>
+                <Col>
+                    <Link href='https://twitter.com/rentfun_io/status/1638731103677841409' target="_blank">
+                        Testing Guide
+                    </Link>
+                </Col>
                 <Col>Arbitrum Goerli Testnet</Col>
                 <Col>
                     <Button variant="primary" onClick={init}>
